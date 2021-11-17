e2ff3ee0fd3a4e05fc99db78867f10e0202b4439
chile <entoyne@posteo.org>
2021-11-17 14:38:02 -0300

    date

--- main.py
+++ main.py
@@ -8,8 +8,8 @@ deathsChile=pd.DataFrame({'date':dataSum.index,'deaths':dataSum.values})
 
 deathsChile['date'] = pd.to_datetime(deathsChile['date'], format = '%Y-%m-%d')
 
-deathsByYears = pd.pivot_table(deathsChile, index=deathsChile.date.dt.strftime('%d-%m'), columns=deathsChile.date.dt.year, values='deaths').reset_index()
-deathsByYearsFrom2015 = deathsByYears.iloc[:,5:]
+deathsByYears = pd.pivot_table(deathsChile, index=deathsChile.date.dt.strftime('%d-%m'), columns=deathsChile.date.dt.year, values='deaths')
+deathsByYearsFrom2015 = deathsByYears.iloc[:,5:].reset_index()
 deathsByYearsFrom2015.to_csv('./output/deathsChileFrom2015.csv', index=False)
 
 data.to_csv('./input/deathsByComunaInChile.csv')
