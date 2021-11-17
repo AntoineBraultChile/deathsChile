b84f086f0e57413da510dba89118ee889c822546
chile <entoyne@posteo.org>
2021-11-17 20:19:12 -0300

    to json

--- main.py
+++ main.py
@@ -4,6 +4,9 @@ import json
 
 data = pd.read_csv('https://raw.githubusercontent.com/MinCiencia/Datos-COVID19/master/output/producto32/Defunciones.csv')
 
+years =  [i for i in list(set(pd.to_datetime(data.iloc[:,4:].columns, format = '%Y-%m-%d').year)) if i > 2014]
+years.sort()
+
 dataSum = data.iloc[:,4:].sum(axis=0)
 deathsChile=pd.DataFrame({'date':dataSum.index,'deaths':dataSum.values})
 
@@ -11,11 +14,16 @@ deathsChile['date'] = pd.to_datetime(deathsChile['date'], format = '%Y-%m-%d')
 
 deathsByYears = pd.pivot_table(deathsChile, index=deathsChile.date.dt.strftime('%m-%d'), columns=deathsChile.date.dt.year, values='deaths')
 deathsByYearsFrom2015 = deathsByYears.iloc[:,5:].reset_index()
-deathsByYearsFrom2015.to_csv('./output/deathsChileFrom2015.csv', index=False)
+
+output = {'Chile':{}}
+for y in years:
+    output['Chile'][y] = {'labels': (str(y)+'-'+deathsByYearsFrom2015['date']).values.tolist(), 'values':deathsByYearsFrom2015[y].values.astype(str).tolist()}
+
+with open('output/deathsChileFrom2015.json', 'w') as json_file:
+    json.dump(output, json_file)
+# deathsByYearsFrom2015.to_csv('./output/deathsChileFrom2015.csv', index=False)
 
 region = data.Region.unique()
-years =  [i for i in list(set(pd.to_datetime(data.iloc[:,4:].columns, format = '%Y-%m-%d').year)) if i > 2014]
-years.sort()
 outputR = {}
 for r in region:
     outputR[r]={}
